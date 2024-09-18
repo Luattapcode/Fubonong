ac55da814e813776e005bc79d35f60801807409f
Luattapcode <luat27508@gmail.com>
2024-09-18 20:49:58 +0700

    Add files via upload

---
+++ templatecualuat.cpp
@@ -0,0 +1,74 @@
+/*The best way to get something done is to begin*/
+#include <bits/stdc++.h>
+#define ll long long
+#define int long long
+#define fr(i,a,b) for(int i=a;i<=b;i++)
+#define frd(i,a,b) for(int i=a;i>=b;i--)
+#define pb push_back
+#define fi first
+#define se second
+#define el '\n'
+#define dembit1(x) __builtin_popcount(x)
+#define TASK "data"
+#define openfile if(fopen(TASK".inp","r")) {freopen(TASK".inp", "r", stdin); freopen(TASK".out", "w", stdout);}
+using namespace std;
+template <class X, class Y>
+	bool minimize(X &x, const Y &y) {
+		if (x > y) {
+			x = y;
+			return true;
+		} else return false;
+	}
+template <class X, class Y>
+	bool maximize(X &x, const Y &y) {
+		if (x < y) {
+			x = y;
+			return true;
+		} else return false;
+	}
+template<class T>
+    T Abs(const T &x) {
+        return (x < 0 ? -x : x);
+    }
+/* Author: Huynh Quoc Luat */
+/*Khai Bao*/
+const long long inf=1e18;
+const int LO=17;
+const int CH=27;
+const int N=1e5+5;
+//void add(int &x,const int &y){x+=y;if(x>=sm)x-=sm;}
+//void sub(int &x,const int &y){x-=y;if(x<0)x+=sm;}
+/*END*/
+/*🥑*/
+void doc()
+{
+
+}
+namespace sub1
+{
+	void xuly()
+	{
+
+	}
+}
+namespace sub2
+{
+	void xuly()
+	{
+
+	}
+}
+void time()
+{
+	cerr<< endl<<"Luattapcode: "<<clock()<<"ms"<<endl;
+}
+main()
+{
+	ios_base::sync_with_stdio(NULL);cin.tie(NULL);cout.tie(NULL);
+	openfile
+	//freopen(TASK".INP", "r", stdin);
+	//freopen(TASK".OUT", "w", stdout);
+	doc();
+	sub1::xuly();
+	time();
+}
